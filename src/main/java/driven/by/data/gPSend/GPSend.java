46df77c6c837371bea74d5f859bc7d1f6a7e231c
Brihtec <97535144+BrihtaKai@users.noreply.github.com>
2024-12-07 12:35:24 +0100

    Default

---
+++ src/main/java/driven/by/data/gPSend/GPSend.java
@@ -0,0 +1,17 @@
+package driven.by.data.gPSend;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GPSend extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
